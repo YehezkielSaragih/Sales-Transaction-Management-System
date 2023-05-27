f0e459254ea98eeba74f362b8b03e77e9627a59a
Yehezkiel Stephanus Austin <105846814+YehezkielSaragih@users.noreply.github.com>
2023-05-27 07:44:46 +0700

    Transaksi tambah card

--- resources/views/transaksi/transaksi_table.blade.php
+++ resources/views/transaksi/transaksi_table.blade.php
@@ -42,6 +42,52 @@
             </div>
         </div>
     </nav>
+    <!-- Form Card -->
+    <div class="container">
+        <div class="card">
+            <div class="card-header">Tambah Data Transaksi</div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <!-- Error Message -->
+                    @if(Session::has('error'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ Session::get('error') }}
+                        </div>
+                    @endif
+                    <!-- Success Message -->
+                    @if(Session::has('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('success') }}
+                        </div>
+                    @endif
+                    <!-- Create function redirect back to /transaksi/transaksi_table -->
+                    <form action="" method="POST">
+                        @csrf
+                        <!-- Static Form Fields -->
+                        <div class="form-group">
+                            <label for="tanggal">Tanggal Transaksi</label>
+                            <input type="date" class="form-control" id="tanggal" name="tanggal" required>
+                        </div>
+                        <!-- Dynamic Form Fields -->
+                        <div class="card mb-3" id="dynamic-form">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <label for="barang">Nama Barang</label>
+                                    <input type="text" class="form-control" name="nama_barang[]" required>
+                                    <label for="jumlah-barang">Jumlah Barang</label>
+                                    <input type="number" class="form-control" name="jumlah_barang[]" required>
+                                    <button class="btn btn-danger" onclick="removeField(event)">Delete</button>
+                                </li>
+                            </ul>
+                        </div>
+                        <button type="button" class="btn btn-primary" onclick="addField()">Add Another</button>
+                        <!-- Submit Button -->
+                        <button type="submit" class="btn btn-success">Tambah</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
 
     <!-- Main Table -->
     <div class="container">
@@ -71,6 +117,7 @@
 
     <!-- Script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="{{ URL::asset('js/dynamic_form.js') }}"></script>
 
 </body>
 
