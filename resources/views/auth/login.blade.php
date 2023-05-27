6b4e3c3a1b470cc687456bad8b165774d167a7a4
YehezkielSaragih <yehezkiel.stephanus@gmail.com>
2023-05-27 18:22:55 +0700

    Authpage background

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,11 +8,12 @@
     <title>Toko Aneka ATK - Login Page</title>
     <!-- Style -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body>
+<body id="authpage">
     
-    <div class="row justify-content-center mt-5">
+    <div class="d-flex justify-content-center align-items-center h-100">
         <div class="col-lg-4">
             <div class="card">
                 <div class="card-header">
